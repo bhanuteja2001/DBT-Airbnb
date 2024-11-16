3d38d2755514787062135e1db7739acb7cfd4777
Bhanu Teja <44323155+bhanuteja2001@users.noreply.github.com>
2024-11-16 03:04:20 +0000

    done till packages and macros

--- dbtlearn/target/compiled/dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/target/compiled/dbtlearn/models/fct/fct_reviews.sql
@@ -18,8 +18,19 @@ FROM
 ), src_reviews AS (
   SELECT * FROM __dbt__cte__src_reviews
 )
-SELECT * FROM src_reviews
+SELECT 
+  md5(cast(coalesce(cast(listing_id as 
+    varchar
+), '') || '-' || coalesce(cast(review_date as 
+    varchar
+), '') || '-' || coalesce(cast(reviewer_name as 
+    varchar
+), '') || '-' || coalesce(cast(review_text as 
+    varchar
+), '') as 
+    varchar
+))
+    AS review_id,
+  * 
+  FROM src_reviews
 WHERE review_text is not null
-
-
-  AND review_date > (select max(review_date) from AIRBNB.DEV.fct_reviews)
--- dbtlearn/target/run/dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/target/run/dbtlearn/models/fct/fct_reviews.sql
@@ -1,11 +1,46 @@
--- back compat for old kwarg name
+
   
-  begin;
     
 
-        insert into AIRBNB.DEV.fct_reviews ("LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT")
+        create or replace transient table AIRBNB.DEV.fct_reviews
+         as
         (
-            select "LISTING_ID", "REVIEW_DATE", "REVIEWER_NAME", "REVIEW_TEXT", "REVIEW_SENTIMENT"
-            from AIRBNB.DEV.fct_reviews__dbt_tmp
+WITH  __dbt__cte__src_reviews as (
+WITH raw_reviews AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.raw.raw_reviews
+)
+SELECT
+    LISTING_ID,
+    DATE AS REVIEW_DATE,
+    REVIEWER_NAME,
+    COMMENTS AS REVIEW_TEXT,
+    SENTIMENT AS REVIEW_SENTIMENT
+    
+FROM
+    raw_reviews
+), src_reviews AS (
+  SELECT * FROM __dbt__cte__src_reviews
+)
+SELECT 
+  md5(cast(coalesce(cast(listing_id as 
+    varchar
+), '') || '-' || coalesce(cast(review_date as 
+    varchar
+), '') || '-' || coalesce(cast(reviewer_name as 
+    varchar
+), '') || '-' || coalesce(cast(review_text as 
+    varchar
+), '') as 
+    varchar
+))
+    AS review_id,
+  * 
+  FROM src_reviews
+WHERE review_text is not null
+
         );
-    commit;
+      
+  
